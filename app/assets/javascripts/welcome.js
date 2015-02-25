d35bfa3ae2b73384d3da2e39d137ff2474915a79
Simeon Georgiev <s.georgiev.27@gmail.com>
2015-02-25 00:53:43 -0600

    one page design

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -1,4 +1,5 @@
 var homeLoad = function() {
+  // handlers for modals
   $("#calcOverlay").click(function() {
     $(".coverlay").fadeIn(200);
     $(".cmodal").fadeIn(200);
@@ -7,12 +8,44 @@ var homeLoad = function() {
   $("#sudokuOverlay").click(function() {
     $(".coverlay").fadeIn(200);
     $(".smodal").fadeIn(200);
+    $('html, body').animate({
+        scrollTop: $("#portfolio").offset().top - 50
+    }, 800);
   });
 
   $(".coverlay").click(function() {
     $(".coverlay").fadeOut(200);
     $(".cmodal").fadeOut(200);
     $(".smodal").fadeOut(200);
+  });
+
+  // smooth scroll
+  $("#stotop").click(function(event) {
+    event.preventDefault();
+    $("html, body").animate({
+      scrollTop: 0
+    }, 800);
+  });  
+
+  $("#stoabt").click(function(event) {
+    event.preventDefault();
+    $('html, body').animate({
+        scrollTop: $("#about").offset().top
+    }, 800);
+  });
+
+  $("#stoport").click(function(event) {
+    event.preventDefault();
+    $('html, body').animate({
+        scrollTop: $("#portfolio").offset().top - 50
+    }, 800);
+  });
+
+  $("#stocont").click(function(event) {
+    event.preventDefault();
+    $('html, body').animate({
+        scrollTop: $("#contact").offset().top - 50
+    }, 800);
   });
 };
 
